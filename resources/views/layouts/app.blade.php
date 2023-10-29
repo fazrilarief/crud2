75f8b3b3e1d63f6e412a394af94f3b2a4cc72521
Fazril Arief Nugraha <fazrilarief331@gmail.com>
2023-10-29 21:39:46 +0700

    Membuat Project Laravel dan View Data Siswa

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('includes.head')
+</head>
+
+<body>
+    @yield('content')
+    @include('includes.script')
+</body>
+
+</html>
---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <hr>
+        <h4 class="text-center">Data Siswa</h4>
+        <hr>
+        <div class="card">
+            <div class="card-header">
+                <a href="#" class="btn btn-primary">Tambah Data</a>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>NIM</th>
+                            <th>Nama</th>
+                            <th>Jurusan</th>
+                            <th>Email</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
